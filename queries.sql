0e0e35e52e4c6b5868d6abf40a155be5c624b721
unknown <Dustin.Guillen14@yahoo.com>
2020-05-28 19:23:44 -0600

    updated queries to northwind database

--- queries.sql
+++ queries.sql
@@ -1,33 +1,32 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-SELECT p.ProductName, c.CategoryName
-from Products AS p
-join Categories AS c
-    on p.CategoryId = c.CategoryId;
+select p.ProductName, c.CategoryName
+from Product as p
+join Category as c
+    on p.CategoryId = c.Id;
 
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-SELECT o.OrderId, s.ShipperName, o.OrderDate
-from Orders AS o 
-join Shippers AS s
-    on o.ShipperID = s.ShipperId
+select o.Id, s.CompanyName
+from [Order] as o 
+join Shipper as s
+    on o.ShipVia = s.Id
 where o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-SELECT p.ProductName, od.Quantity
-from OrderDetails AS od
-join Products AS p
-    on od.ProductId = p.ProductId
-    where od.OrderId = 10251
+select p.ProductName, od.Quantity
+from OrderDetail as od
+join Product as p
+    on od.ProductId = p.Id
+    where od.OrderId=10251
     order by p.ProductName;
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
-
-SELECT o.OrderId AS 'OrderId', c.CustomerName AS 'CompanyName', e.LastName AS 'EmployeeLastName'
-from Orders AS o
-join Customers AS c
-	on o.CustomerId = c.CustomerId
-join Employees AS e
-    on o.EmployeeId = e.EmployeeId
+select DISTINCT o.Id as 'Order Id', c.CompanyName as 'Company Name', e.LastName as 'Employee Last Name'
+from [Order] as o
+join Customer as c
+join Employee as e
+    on o.CustomerId = c.Id
+    where o.EmployeeId = e.Id;
